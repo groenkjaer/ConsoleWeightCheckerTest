ba15c78aea4195b6cf0af4942f3daae852a7c10c
Fillip <fill0246@student.eadania.dk>
2020-01-12 20:16:32 +0100

    Add project files.

---
+++ ConsoleWeightChecker/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace ConsoleWeightChecker
+{
+    class Program
+    {
+        static SerialPort _serialPort;
+        static void Main(string[] args)
+        {
+            _serialPort = new SerialPort();
+            SerialCommunication.ConnectToSerial(_serialPort);
+            
+            while (SerialCommunication.ItWorked)
+            {
+                _serialPort.Write("p");
+                _serialPort.Write("0");
+                Console.WriteLine(_serialPort.ReadExisting());
+
+                System.Threading.Thread.Sleep(2000);
+            }
+        }
+    }
+}
---
+++ ConsoleWeightChecker/SerialCommunication.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace ConsoleWeightChecker
+{
+    static class SerialCommunication
+    {
+        private static bool itWorked = false;
+
+        public static bool ItWorked
+        {
+            get { return itWorked; }
+        }
+
+        public static void ConnectToSerial(SerialPort _serialPort)
+        {
+            while (!SerialCommunication.ItWorked) //Keep trying
+            {
+                try
+                {
+                    _serialPort.BaudRate = 19200;
+                    _serialPort.Parity = Parity.None;
+                    _serialPort.DataBits = 8;
+                    _serialPort.StopBits = StopBits.One;
+                    _serialPort.Handshake = Handshake.RequestToSend;
+                    Console.WriteLine("Input COM Number");
+                    _serialPort.PortName = "COM" + Console.ReadLine().ToString();
+                    _serialPort.Open();
+                    itWorked = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Somethings wrong");
+                }
+            }
+        }
+    }
+}
